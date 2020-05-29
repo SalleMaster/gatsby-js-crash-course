18236a7ca9ddc3a7a5d6639520c39412481617d3
Sasa Radovanovic <salle90ar@gmail.com>
2020-05-29 13:03:21 +0200

    Blog Page

---
+++ src/pages/services.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Services = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Services</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit natus
+      accusamus, totam reprehenderit ea hic laudantium nostrum repudiandae
+      debitis eveniet est sed corporis, ex nisi et? Eligendi laborum ab tempora
+      quo enim, ex provident explicabo maxime et labore? Laborum, facilis.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit natus
+      accusamus, totam reprehenderit ea hic laudantium nostrum repudiandae
+      debitis eveniet est sed corporis, ex nisi et? Eligendi laborum ab tempora
+      quo enim, ex provident explicabo maxime et labore? Laborum, facilis.
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit natus
+      accusamus, totam reprehenderit ea hic laudantium nostrum repudiandae
+      debitis eveniet est sed corporis, ex nisi et? Eligendi laborum ab tempora
+      quo enim, ex provident explicabo maxime et labore? Laborum, facilis.
+    </p>
+
+    {/* <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
+  </Layout>
+)
+
+export default Services
